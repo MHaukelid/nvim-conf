c9b23b7183ff515e947f7148da022094d12fee30
Marcus Haukelid Larsen <mhl@haukelid.dk>
2024-05-08 13:24:56 +0200

    Config update

--- lua/init-lsp.lua
+++ lua/init-lsp.lua
@@ -1,35 +1,111 @@
+local cmp = require("cmp")
+
+cmp.setup({
+    snippet = {
+        -- REQUIRED - you must specify a snippet engine
+        expand = function(args)
+            vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+        end,
+    },
+    
+    window = {
+      -- completion = cmp.config.window.bordered(),
+      -- documentation = cmp.config.window.bordered(),
+    },
+    
+    mapping = cmp.mapping.preset.insert({
+        ["<Tab>"] = cmp.mapping.confirm({ select = false }),
+        ["<Esc>"] = cmp.mapping.abort(),
+        ["<Up>"] = cmp.mapping.select_prev_item({ behavior = "select" }),
+        ["<Down>"] = cmp.mapping.select_next_item({ behavior = "select" }),
+    }),
+
+    sources = cmp.config.sources({
+        { name = 'nvim_lsp' },
+        { name = 'vsnip' }, -- For vsnip users.
+    }, {
+        { name = 'buffer' },
+    })
+})
+
+-- To use git you need to install the plugin petertriho/cmp-git and uncomment lines below
+-- Set configuration for specific filetype.
+--[[ cmp.setup.filetype('gitcommit', {
+sources = cmp.config.sources({
+  { name = 'git' },
+}, {
+  { name = 'buffer' },
+})
+})
+require("cmp_git").setup() ]]-- 
+
+-- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline({ '/', '?' }, {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = {
+        { name = 'buffer' }
+    }
+})
+
+-- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline(':', {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = cmp.config.sources({
+        { name = 'path' }
+    }, {
+        { name = 'cmdline' }
+    }),
+
+    matching = { disallow_symbol_nonprefix_matching = false }
+})
+
+-- Set up lspconfig.
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
 require("lsp-zero").setup({})
 local lsp_config = require("lspconfig")
 
 lsp_config.lua_ls.setup({
-  settings = {
-    Lua = {
-      runtime = {
-        -- Tell the language server which version of Lua you're using
-        -- (most likely LuaJIT in the case of Neovim)
-        version = "LuaJIT",
-      },
-      diagnostics = {
-        -- Get the language server to recognize the `vim` global
-        globals = {
-          "vim",
-          "require"
+    settings = {
+        Lua = {
+            runtime = {
+                -- Tell the language server which version of Lua you're using
+                -- (most likely LuaJIT in the case of Neovim)
+                version = "LuaJIT",
+            },
+            diagnostics = {
+                -- Get the language server to recognize the `vim` global
+                globals = {
+                "vim",
+                "require"
+                },
+            },
+            workspace = {
+                -- Make the server aware of Neovim runtime files
+                library = vim.api.nvim_get_runtime_file("", true),
+            },
+            -- Do not send telemetry data containing a randomized but unique identifier
+            telemetry = {
+                enable = false,
+            },
         },
-      },
-      workspace = {
-        -- Make the server aware of Neovim runtime files
-        library = vim.api.nvim_get_runtime_file("", true),
-      },
-      -- Do not send telemetry data containing a randomized but unique identifier
-      telemetry = {
-        enable = false,
-      },
     },
-  },
+
+    capabilities = capabilities
 })
 
-lsp_config.gopls.setup({})
-lsp_config.rust_analyzer.setup({})
-lsp_config.terraformls.setup({})
-lsp_config.pylsp.setup({})
-lsp_config.bashls.setup({})
+lsp_config.gopls.setup({ capabilities = capabilities })
+lsp_config.rust_analyzer.setup({ capabilities = capabilities })
+lsp_config.terraformls.setup({ capabilities = capabilities })
+lsp_config.pylsp.setup({ capabilities = capabilities })
+lsp_config.bashls.setup({ capabilities = capabilities })
+
+vim.api.nvim_create_autocmd({ "BufEnter" }, {
+  pattern = { "*.bb", "*.bbappend", "*.bbclass", "*.inc", "conf/*.conf" },
+  callback = function()
+    vim.lsp.start({
+      name = "bitbake",
+      cmd = { "bitbake-language-server" }
+    })
+  end,
+})
